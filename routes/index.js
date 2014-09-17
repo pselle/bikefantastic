ddefa60071dc39e01db750bcdd7d59e259957278
Pamela Selle <pamela_selle@cable.comcast.com>
2014-09-17 16:54:09 -0500

    Cleanup

--- routes/index.js
+++ routes/index.js
@@ -5,7 +5,6 @@
 var fs = require('fs');
 
 exports.index = function(req, res){
-  var bikes = [];
   fs.readdir('./public/images/bikes', function(err, files) {
     res.render('index', { title: 'BikeFantastic', bikes: files });
   });
